9e1ef7db84e5fca40f49cc30f3d23e6e495926b8
anxnas <106204348+anxnas@users.noreply.github.com>
2024-09-05 20:38:58 +0300

    Добавлены модели Task и Category с кастомными первичными ключами

---
+++ README.md
@@ -0,0 +1,25 @@
+django_backend/
+├── todo_list/
+│   ├── __init__.py+
+│   ├── settings.py+
+│   ├── urls.py+
+│   ├── wsgi.py+
+├── tasks/
+│   ├── __init__.py+
+│   ├── models.py+
+│   ├── views.py-
+│   ├── serializers.py-
+│   ├── admin.py-
+│   ├── tests.py-
+├── users/
+│   ├── __init__.py
+│   ├── models.py
+│   ├── views.py
+│   ├── serializers.py
+├── api/
+│   ├── __init__.py
+│   ├── urls.py
+│   ├── views.py
+├── manage.py
+├── Dockerfile
+├── requirements.txt
--- django_backend/tasks/models.py
+++ django_backend/tasks/models.py
@@ -1,3 +1,35 @@
+import time
+import hashlib
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CustomPKModel(models.Model):
+    id = models.CharField(max_length=64, primary_key=True, editable=False) # Уникальный идентификатор
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            timestamp = int(time.time() * 1000)
+            hash_object = hashlib.sha256(str(timestamp).encode())
+            self.id = hash_object.hexdigest()[:64]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+class Category(CustomPKModel):
+    name = models.CharField(max_length=100) # Названия категории
+
+    def __str__(self):
+        return self.name
+
+class Task(CustomPKModel):
+    title = models.CharField(max_length=200) # Заголовок задачи
+    description = models.TextField(blank=True) # Описание задачи
+    created_at = models.DateTimeField(auto_now_add=True) # Дата и время создания задачи
+    due_date = models.DateTimeField(null=True, blank=True) # Срок выполнения задачи
+    completed = models.BooleanField(default=False) # Статус выполнения задачи
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks') # Связь с пользователем (ForeignKey). При удалении пользователя удаляются все его задачи.
+    categories = models.ManyToManyField(Category, related_name='tasks') # Связь многие-ко-многим с категориями. Одна задача может иметь несколько категорий, и одна категория может быть у нескольких задач.
+
+    def __str__(self):
+        return self.title
