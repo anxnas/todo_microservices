397009356fc20deb6e32c4d0f606e54b5ca9ff0d
anxnas <106204348+anxnas@users.noreply.github.com>
2024-09-05 21:24:23 +0300

    Реализован основной функционал Django бэкенда

--- django_backend/tasks/admin.py
+++ django_backend/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task, Category
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'completed')
+    list_filter = ('completed', 'created_at')
+    search_fields = ('title', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
